4f873a3d8e21f5d276570805e46b517ef29035f8
Jim Devine <jiim.devine@gmail.com>
2021-01-17 19:46:34 -0500

    adds pipeline steps to torchtext_pipeline file

---
+++ torchtext_practice.py
@@ -0,0 +1,7 @@
+# Steps: 
+
+# 1. Specify how preprocessing should be done -> Fields
+# 2. Use Dataset to load the data -> TabularDataset
+# 3. Contstruct an iterator to do batching and padding -> BucketIterator
+
+
