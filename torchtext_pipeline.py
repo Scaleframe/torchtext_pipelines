0a27b72655f48ae74c4e62ea403e4315fae5acae
Jim Devine <jiim.devine@gmail.com>
2021-01-18 16:37:44 -0500

    Add pipeline step labels, vocab builder, bucketIterator

--- torchtext_pipeline.py
+++ torchtext_pipeline.py
@@ -6,12 +6,18 @@
 
 from torchtext.data import Field, TabularDataset, BucketIterator
 
+
+# 1. Specify how preprocessing should be done:
 tokenize = lambda x: x.split()
+
 
 quote = Field(sequential=True, use_vocab=True, tokenize=tokenize, lower=True)
 score = Field(sequential=True, use_vocab=False)
 
 fields = {'quote': ('q', quote), 'score':('s', score)}
+
+# 2. Use Dataset to lead the data:
+# (See example data files in mydata directory)
 
 # json data format
 train_data, test_data = TabularDataset.splits(
@@ -39,3 +45,16 @@ train_data, test_data = TabularDataset.splits(
 #                                         validation='validation.tsv'
 #                                         format='tsv',
 #                                         fields=fields)
+
+
+# Build vocab from corpus. "Quotes", from example files
+quote.build_vocab(train_data, 
+                  max_size=10000,
+                  min_freq=1)
+
+
+# 3. Construct an iterator to do batching a padding:
+train_iterator, test_iterator = BucketIterator.splits(
+    (train_data, test_data),
+    batch_size=2, 
+    device="cuda")
