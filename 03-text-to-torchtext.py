53f28b980eea98faee04ea5653f86f31089d405b
Jim Devine <jiim.devine@gmail.com>
2021-01-19 18:57:27 -0500

    Text pipeline: read in files, create dataframe, split data, write to output

--- 03-text-to-torchtext.py
+++ 03-text-to-torchtext.py
@@ -13,3 +13,41 @@ from sklearn.model_selection import train_test_split # use for train test splitt
     # WMT'14 English-German data [Medium], 
         # train.en and train.de. 
 
+
+# First we need to get our data read in from our text files. 
+english_text = open('mydata/WMT_train.en', encoding='utf8').read().split('\n')
+# returns list of strings, english sentences
+
+
+german_text = open('mydata/WMT_train.de', encoding='utf8').read().split('\n')
+# returns: list of strings, german sentences.
+
+'''
+We can use pandas dataframe to get a dictionary of text into a useable format for training.
+'''
+
+# First we need to get our text data into a dictionary. 
+raw_data = {"English": [line for line in english_text[1:10000]],
+            "German": [line for line in german_text[1:10000]]}
+# returns: python dictionary
+
+
+# From dictionary we can make DataFrame object:
+df = pd.DataFrame(raw_data, columns=['English', 'German'])
+
+# With dataframe we can use sklearn's train/test/split:
+train, test = train_test_split(df, test_size=0.2)
+
+# Now we can write our train / test split to json:
+train.to_json('train.json', orient='records', lines=True)
+train.to_json('train.json', orient='records', lines=True)
+# returns: json file with structure: 
+#               {"English": english sentence, 
+#                "German": german_sentence}
+
+# Or csv format: 
+train.to_csv('train.csv')
+train.to_csv('train.csv')
+# returns: json file with structure: 
+#               {"English": english sentence, 
+#                "German": german_sentence}
